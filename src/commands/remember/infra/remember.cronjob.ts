324ff2ce1534394c5d46b40ff032785eec375590
Alejandro Garcia Serna <alejandrogarcia@seven4n.com>
2021-12-10 11:35:51 -0500

    Remove batching and retry policy

--- src/commands/remember/infra/remember.cronjob.ts
+++ src/commands/remember/infra/remember.cronjob.ts
@@ -1,33 +1,30 @@
-import { createBatchedPromise, createRetryPolicy } from 'promises-fn-utils';
 import { Client } from 'discord.js';
 import cron from 'node-cron';
 import { getScheduledNotes, removeDueNotes } from './remember.service';
 import { buildNoteMessage } from '../domain/remember.mappers';
 
-const applySendNoteRetryPolicy = createRetryPolicy({
-  retries: 2,
-  retryTime: 200,
-});
-
 export function scheduleBotOperations(client: Client<boolean>) {
-  const fetchChannel = createBatchedPromise(client.channels.fetch);
-
   cron.schedule('* * * * *', async () => {
     try {
       const savedNotes = await getScheduledNotes();
       const notesToSend = savedNotes.map(async (note) => {
         const messageContent = buildNoteMessage(note);
-        const channel = await fetchChannel(note.channelID);
+
+        const guild = await client.guilds.fetch(note.guild);
+        const channel = guild.channels.cache.get(note.channelID);
 
         if (channel && channel.isText()) {
-          const sendNote = applySendNoteRetryPolicy(channel.send);
-          await sendNote(messageContent);
+          await channel.send(messageContent);
         }
       });
 
       await Promise.all(notesToSend);
 
       return removeDueNotes();
-    } catch {}
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log(err.message);
+      }
+    }
   });
 }
